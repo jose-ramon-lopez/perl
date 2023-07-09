17bb6ad76a2816dfd45a68c1ede914d513cdab7d
Jose Ramon Lopez <joseramon.lopez@dxc.com>
2023-07-09 22:18:08 +0200

    + readLog

---
+++ readLog.pl
@@ -0,0 +1,12 @@
+# Leo log desde la última línea
+use IO::Select;
+
+my $filename = "test.txt";
+
+open my $fh, '<', $filename or die "Can't open '$filename': $!";
+$sel = IO::Select->new( $fh );
+
+while(@ready = $sel->can_read) {
+        print <$fh>;
+
+}
